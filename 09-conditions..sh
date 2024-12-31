fab144ffcb9e3fca1e5d0781c997bf7a941e60ec
Algot Sarika <sarika@joindevops.com>
2024-12-31 18:26:52 +0530

    conditions

---
+++ 09-conditions..sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Number=$1
+
+# -gt, -lt. -eq, -ge, -le
+
+if [$Number -gt 200]
+then
+    echo "given number is greater than 200"
+else
+    echo "given number is less than or equal to 200"
+
+fi
