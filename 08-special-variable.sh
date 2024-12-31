edd07d5e91694d5753892975541251079a5e1de3
Algot Sarika <sarika@joindevops.com>
2024-12-31 17:15:11 +0530

    special var

---
+++ 08-special-variable.sh
@@ -0,0 +1,10 @@
+echo "A;; variables passed: $@"
+echo "Number of variables: $#"
+echo "script name: $0"
+echo "present working directory:$pwd"
+echo "Home directory of current user: $HOME"
+echo "which user is running this script: $USER"
+echo "process id or current script: $$"
+sleep 60 &
+
+echo "process id of last command in background: $!"
