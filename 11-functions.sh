3a0de4470da1278befda3caeb3bb5ab07b3ea1a1
Algot Sarika <sarika@joindevops.com>
2025-01-02 23:08:53 +0530

    shell sript

--- 11-functions.sh
+++ 11-functions.sh
@@ -12,7 +12,7 @@ VALIDATE(){
     fi
 }
 
-if [ $USERID  -ne 0 ]
+if [ $USERID -ne 0 ]
 then 
    echo "ERROR:: you must have sudo access to execute this script"
    exit 1 #other then  0
@@ -24,11 +24,10 @@ if [ $1 -ne 0 ]
 then # not installed
     dnf install mysql -y
     VALIDATE $? "Installing MySQL"
+
 else
     echo "mysql is already .. installed"
 fi
-
-
 
 dnf list installed git
 
