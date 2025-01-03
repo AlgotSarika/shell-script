5e37472c34ef04d15a76f640d6dca2fc2507dabf
Algot Sarika <sarika@joindevops.com>
2025-01-03 11:34:50 +0530

    shell script

--- 11-function.sh
+++ 11-function.sh
@@ -3,13 +3,14 @@
 USERID=$(id -u)
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
+    if [ $? -ne 0 ]
     then
-        echo "$2...failure"
+        echo "$2 ..failure"
         exit 1
     else
         echo "$2 .. success"
     fi
+
 }
 
 if [ $USERID -ne 0 ]
@@ -20,12 +21,11 @@ fi
 
 dnf list installed mysql
 
-if [ $1 -ne 0 ]
+if [ $? -ne 0 ]
 then # not installed
     dnf install mysql -y
-    VALIDATE $? "Installing MySQL"
-
-else
+    VALIDATE $? "Installing MYSQL"
+else 
     echo "mysql is already .. installed"
 fi
 
@@ -33,10 +33,8 @@ dnf list installed git
 
 if [ $? -ne 0 ]
 then #not installed
-
     dnf install git -y
     VALIDATE $? "Installing Git"
-   
 else
-    echo "git is already .. installed"
+    echo "Git is already .. installed"
 fi
