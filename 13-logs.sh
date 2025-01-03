9107d63a71e60eea4473c2a7047d52c72d53a6ca
Algot Sarika <sarika@joindevops.com>
2025-01-03 12:37:46 +0530

    logs

--- 13-logs.sh
+++ 13-logs.sh
@@ -20,7 +20,7 @@ VALIDATE(){
 
 }
 
-echo "script strated executing at : $TIMESTAMP" &>>$LOG_FILE
+echo "script strated executing at : $TIMESTAMP" &>>$LOG_FILE_NAME
 
 if [ $USERID -ne 0 ]
 then 
@@ -28,21 +28,21 @@ then
    exit 1 #other then  0
 fi
 
-dnf list installed mysql &>>$LOG_FILE
+dnf list installed mysql &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then # not installed
-    dnf install mysql -y &>>$LOG_FILE
+    dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing MYSQL"
 else 
     echo -e "mysql is already .. $Y installed"
 fi
 
-dnf list installed git &>>$LOG_FILE
+dnf list installed git &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then #not installed
-    dnf install git -y &>>$LOG_FILE
+    dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing Git"
 else
     echo -e "Git is already .. $Y installed"
