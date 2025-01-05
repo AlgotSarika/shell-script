dfd2b23033475dd4a10ba2ba957cc6d880a9a713
Algot Sarika <sarika@joindevops.com>
2025-01-05 10:48:05 +0530

    expense-shell

---
+++ 18-word_count.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]
+then
+    echo "usage: $0 <file"
+    exit 1
+fi
+
+if [ ! -f "$1" ]
+then
+    echo "ERROR:File '$1' not found."
+    exit 1
+fi
+
+#process the file and count word occurrences
+echo "Top 5 most frequent words:"
+tr -c '[:alnum:]' '[\n*]' < "$1" | tr '[:upper:]' '[:lower:]' | grep -v '^$' | sort | uniq -c | sort -nr | head -n 5 
