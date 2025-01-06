2f68959f760e2275e64dde573c0a0fbe7a547da4
Algot Sarika <sarika@joindevops.com>
2025-01-06 22:11:17 +0530

    backup

--- 20-backup.sh
+++ 20-backup.sh
@@ -60,7 +60,7 @@ if [ -n "$FILES" ] # true if there are files to zip
 then
     echo "Files are: $FILES"
     ZIP_FILE="$DEST_DIR/app-log-$TIMESTAMP.zip"
-    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$FILES"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
 else
     echo "No files found older then $DAYS"
 
