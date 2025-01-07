915e7972f97a6547b6f9b942ff6dc8113b295356
Algot Sarika <sarika@joindevops.com>
2025-01-07 21:54:02 +0530

    backup

--- 20-backup.sh
+++ 20-backup.sh
@@ -12,7 +12,7 @@ DAYS=${3:-14}  #if user is not providing number of days, we are taking 14 as def
 
 LOGS_FOLDER="/home/ec2-user/shellscript-logs"
 LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
-TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 
