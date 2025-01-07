3bb4bc6101200ad852a75ca145d90db8726ab8e3
Algot Sarika <sarika@joindevops.com>
2025-01-08 02:38:00 +0530

    script1

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -36,9 +36,11 @@ echo "script strated executing at : $TIMESTAMP" &>>$LOG_FILE_NAME
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files to be deleted: $FILES_TO_DELETE"
 
-while read -r file
+while read -r filepath #here filepath is the variale name, we can give any name
 do
-    echo "Deleting file: $file"
+    echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
+
     rm -rf $file
+    echo "Deleting file: $filepath"
 
 done  <<< $FILES_TO_DELETE
