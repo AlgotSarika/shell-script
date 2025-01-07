8f09816a21a2fb4102eb3349f40fabbeddba03dd
Algot Sarika <sarika@joindevops.com>
2025-01-08 01:42:49 +0530

    email

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -5,8 +5,8 @@ DISK_THRESHOLD=5 #real projects will monitor for 70
 
 while read -r line
 do
-    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    USAGE=$(echo $line | awk -F " " '{print $6F}' |cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
     echo "partition:$PARTITION, usage: $USAGE"
-    
+
 done <<< $DISK_USAGE
