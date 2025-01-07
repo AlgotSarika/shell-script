7a6980cb0074153b461040f73f3b2626c4fc9b4a
Algot Sarika <sarika@joindevops.com>
2025-01-08 01:47:59 +0530

    email

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -7,6 +7,10 @@ while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' |cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "partition:$PARTITION, usage: $USAGE"
-
+    #echo "partition:$PARTITION, usage: $USAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="High Disk usage in partition: $PARTITION usage is: $USAGE"
 done <<< $DISK_USAGE
+
+echo  "Message:$MSG"
